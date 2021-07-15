b14b9d0eadc7e5eaa6470aaecd9263773a0e9afb
Guriya_kumari <81032463+guriyakumari551@users.noreply.github.com>
2021-07-15 23:28:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+import Appli from './Appli';
+
+
+ReactDOM.render(
+
+<Appli/>,
+ document.getElementById("root")
+);
